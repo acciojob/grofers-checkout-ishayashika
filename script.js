85a1f1cafb00f518ec9c5af5a38a75dd7bd039dc
ishayashika <122111684+ishayashika@users.noreply.github.com>
2025-02-01 16:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,28 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
-  
+	let sum=0;
+	let total=document.querySelectorAll(".price");
+	total.forEach((item)=>{
+		sum+=parseFloat(item.textContent);
+	})
+	//create row
+	const table = document.getElementById("priceTable");
+	const tBody = table.querySelector("tbody");
+
+    const totalRow = document.createElement("tr");
+
+
+	const totalCell = document.createElement("td");
+    totalCell.textContent = "Total Price";
+
+	const priceCell = document.createElement("td");
+    priceCell.textContent = sum.toFixed(2);  // Display total with 2 decimal places
+
+    totalRow.appendChild(totalCell);
+    totalRow.appendChild(priceCell);
+
+	tBody.appendChild(totalRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
